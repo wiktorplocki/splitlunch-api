06dd091be35205e3190969857ea15c5abb53bc59
wiktor Płocki <wiktor.plocki@gmail.com>
2019-10-05 16:41:56 +0200

    Add email validation, tokenVersion

--- src/models/user.js
+++ src/models/user.js
@@ -5,12 +5,22 @@ const {
 } = mongoose.Schema;
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true },
+  email: {
+    type: String,
+    validate: {
+      validator: value =>
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          value
+        ),
+      message: props => `${props.value} is not a valid email address!`
+    },
+    required: [true, 'Email address required']
+  },
   password: { type: String, required: true },
   name: { type: String },
   balance: { type: Number, default: 0 },
   orders: [{ type: ObjectId, ref: 'Order' }],
-  count: { type: Number, default: 0 }
+  tokenVersion: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('User', UserSchema);
