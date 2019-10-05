3d6844fc3fc8fcbb606755d6a179b0f77212a440
wiktor Płocki <wiktor.plocki@gmail.com>
2019-10-05 16:40:56 +0200

    Remove unneeded comment

--- src/helpers/createTokens.js
+++ src/helpers/createTokens.js
@@ -1,20 +1,5 @@
 const { sign } = require('jsonwebtoken');
 
-// const createTokens = user => {
-//   const refreshToken = jsonwebtoken.sign(
-//     { userId: user.id, count: user.count },
-//     process.env.REFRESH_TOKEN_SECRET,
-//     { expiresIn: '7d' }
-//   );
-//   const accessToken = jsonwebtoken.sign(
-//     { userId: user.id, count: user.count },
-//     process.env.ACCESS_TOKEN_SECRET,
-//     { expiresIn: '15m' }
-//   );
-
-//   return { refreshToken, accessToken };
-// };
-
 const createAccessToken = user => {
   return sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: '15m'
